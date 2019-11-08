9205cc93e7399f30d9dbf912603c4c107b50c972
takayuki takahashi <ttkaws15@gmail.com>
2019-11-08 14:18:02 +0900

    Add: stationモデルにnotnull制約追加

---
+++ db/migrate/20191108051338_change_column_to_not_null.rb
@@ -0,0 +1,7 @@
+class ChangeColumnToNotNull < ActiveRecord::Migration[5.0]
+  def change
+    change_column :stations, :route,:string, null: false
+    change_column :stations, :name,:string, null: false
+    change_column :stations, :time_needed,:integer, null: false
+  end
+end
